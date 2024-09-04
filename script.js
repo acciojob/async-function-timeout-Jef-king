83564279fa32af92949b531e842ed2ded18582e7
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-09-04 13:29:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// function delay(ms){
+// 	return new Promise(resolve=>setTimeout(resolve,ms));
+// }
+// asyn function act(){
+// 	let txt=document.querySelector("#text").value;
+// 	let time=parseInt(document.querySelector("#delay").value,10);
+// 	document.querySelector("#output").innerHTML=txt;
+// }
+function action(){
+	document.querySelector("#output").innerHTML=document.querySelector("#text").value;
+}
+document.querySelector("#btn").addEventListener("click",action);
+
